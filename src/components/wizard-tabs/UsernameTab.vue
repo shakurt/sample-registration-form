1d0660570873bed7e698d37f171a2bf4c8e7bb78
Shak <shakurtousian@gmail.com>
2024-05-06 19:57:02 +0330

    add usernameTab component

---
+++ src/components/wizard-tabs/UsernameTab.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+defineProps({
+  error: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+const username = defineModel({ type: String, required: true })
+</script>
+
+<template>
+  <div class="mx-auto flex w-9/12 flex-col">
+    <label for="username" class="mb-2 text-lg text-white">Enter username:</label>
+    <input v-model="username" id="username" name="username" autofocus class="input" type="text" />
+    <Transition name="fade-preset" mode="out-in">
+      <p v-if="error" class="mt-1 text-sm text-red-500">{{ error }}</p>
+    </Transition>
+  </div>
+</template>
