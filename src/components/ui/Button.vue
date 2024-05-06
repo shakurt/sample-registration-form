3ada866902997bb58e0a77fe010e4cbe842a6ab8
Shak <shakurtousian@gmail.com>
2024-05-06 19:57:51 +0330

    add disabled prop to button component

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -7,26 +7,31 @@ defineProps({
   type: {
     type: String,
     required: true
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false
   }
 })
 </script>
 
 <template>
-  <button :type class="button" :class="variant">
+  <button :type class="button" :disabled :class="variant">
     <slot />
   </button>
 </template>
 
 <style scoped lang="css">
 .button {
-  @apply min-w-16 rounded-full px-5 py-2 text-white outline-none transition-all;
+  @apply min-w-16 rounded-full px-5 py-2 text-base font-bold text-white outline-none transition-all;
 }
 
 .button.primary {
-  @apply bg-secondary font-bold hover:bg-secondary-700;
+  @apply bg-secondary   disabled:bg-secondary-400 [&:not([disabled])]:hover:bg-secondary-700;
 }
 
 .button.outline {
-  @apply border-2 border-secondary  hover:bg-secondary;
+  @apply border-2 border-secondary  [&:not([disabled])]:hover:bg-secondary;
 }
 </style>
