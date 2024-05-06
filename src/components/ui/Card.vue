161643678a84ebf4f688752bb64075122431ace9
Shak <shakurtousian@gmail.com>
2024-05-06 20:54:07 +0330

    change padding of card component for small view-ports

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="h-96 w-full max-w-[1000px] rounded-2xl p-10 shadow-2xl">
+  <article class="h-96 w-full max-w-[1000px] rounded-2xl p-5 shadow-2xl md:p-10">
     <slot />
   </article>
 </template>
