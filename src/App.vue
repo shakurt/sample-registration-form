7edf5d8deb27773799d3f86ae930a14f3992817d
Shak <shakurtousian@gmail.com>
2024-05-06 17:42:47 +0330

    make items center to page in app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<script setup>
+<script setup lang="js">
 import { RouterView } from "vue-router";
 </script>
 
 <template>
-	<div aria-label="a wrapper for our pages" class="container">
+	<div
+		aria-label="a wrapper for our pages to center items"
+		class="grid place-items-center h-screen container bg-secondary"
+	>
 		<RouterView />
 	</div>
 </template>
