e0549e426baa59ce75abea9d77ffed28e3ea4545
Shak <shakurtousian@gmail.com>
2024-05-06 20:53:14 +0330

    complete description component

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,3 +1,21 @@
+<script setup lang="js">
+defineProps({
+  userInfo: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
 <template>
-  
+  <div class="">
+    <p class="text-xl text-white">
+      Username:
+      <span class="font-bold">{{ userInfo.username }}</span>
+    </p>
+    <p class="text-xl text-white">
+      Email:
+      <span class="font-bold">{{ userInfo.email }}</span>
+    </p>
+  </div>
 </template>
