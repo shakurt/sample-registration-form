96ccdc82e4b38a6e7b7d2507bd13f04e750fb799
Shak <shakurtousian@gmail.com>
2024-05-06 19:57:13 +0330

    add EmailTab component

---
+++ src/components/wizard-tabs/EmailTab.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+defineProps({
+  error: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+const email = defineModel({ type: String, required: true })
+</script>
+
+<template>
+  <div class="mx-auto flex w-9/12 flex-col">
+    <label for="email" class="mb-2 text-lg text-white">Enter email:</label>
+    <input v-model="email" id="email" name="email" autofocus class="input" type="text" />
+    <Transition name="fade-preset" mode="out-in">
+      <p v-if="error" class="mt-1 text-sm text-red-500">{{ error }}</p>
+    </Transition>
+  </div>
+</template>
