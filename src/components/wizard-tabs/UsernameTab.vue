5eedfbaaf3e04241fb42a65322ce4db230f61454
Shak <shakurtousian@gmail.com>
2024-05-06 20:44:29 +0330

    complete username tab component

--- src/components/wizard-tabs/UsernameTab.vue
+++ src/components/wizard-tabs/UsernameTab.vue
@@ -4,6 +4,11 @@ defineProps({
     type: String,
     required: false,
     default: ''
+  },
+  valid: {
+    type: String,
+    required: false,
+    default: ''
   }
 })
 
@@ -12,10 +17,11 @@ const username = defineModel({ type: String, required: true })
 
 <template>
   <div class="mx-auto flex w-9/12 flex-col">
-    <label for="username" class="mb-2 text-lg text-white">Enter username:</label>
+    <label for="username" class="mb-2 text-lg text-white">Username:</label>
     <input v-model="username" id="username" name="username" autofocus class="input" type="text" />
     <Transition name="fade-preset" mode="out-in">
       <p v-if="error" class="mt-1 text-sm text-red-500">{{ error }}</p>
+      <p v-else-if="valid" class="mt-1 text-sm text-green-500">{{ valid }}</p>
     </Transition>
   </div>
 </template>
