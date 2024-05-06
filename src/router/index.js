d1d92786914ef9cf98aabbdfced95575cba34ad4
Shak <shakurtousian@gmail.com>
2024-05-07 02:18:24 +0330

    add catch all 404 page router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/",
-			name: "home",
-			component: HomeView,
-		},
-	],
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    { path: '/:pathMatch(.*)*', component: NotFoundView }
+  ]
+})
 
-export default router;
+export default router
