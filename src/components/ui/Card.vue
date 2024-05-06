06ddaa13b858744ed9aaba29a11c56a8f82fcc2c
Shak <shakurtousian@gmail.com>
2024-05-06 19:57:29 +0330

    add shadow to card component

--- src/components/ui/Card.vue
+++ src/components/ui/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="h-96 w-full max-w-[1000px] rounded-xl p-10">
+  <article class="h-96 w-full max-w-[1000px] rounded-2xl p-10 shadow-2xl">
     <slot />
   </article>
 </template>
